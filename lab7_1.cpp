d574dc666b28c369ec94bde263d8ddaecabb1d0b
prakao <kpkrisrin@gmail.com>
2025-12-12 14:56:11 +0700

    lab7_1_complete

--- lab7_1.cpp
+++ lab7_1.cpp
@@ -1,10 +1,6 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-    int a  =  20; b = 30;
-    int c = 4, output;
-
-    outpot = a - b/c;
-    count << output: << output << \n;
-    return 0;
-}
+    double a  =  20, b = 30;
+    
