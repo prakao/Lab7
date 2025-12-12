686abafe6dec8f156e85af44f12db9bcb66f6a47
prakao <kpkrisrin@gmail.com>
2025-12-12 14:58:06 +0700

    Lab7_1_complete

--- lab7_1.cpp
+++ lab7_1.cpp
@@ -3,4 +3,9 @@ using namespace std;
 
 int main() {
     double a  =  20, b = 30;
-    
+    double c = 4, output;
+
+    output = (a + b)/c;
+    cout << "output: "<< output << "\n";
+    return 0;
+}
